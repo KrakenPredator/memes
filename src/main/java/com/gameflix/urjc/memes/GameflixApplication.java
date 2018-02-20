aac7f087bf5c4fc5262d7cd1d975b7390c9d080d
Javier <javichi96@gmail.com>
2018-02-20 16:33:52 +0100

    Initial commit

---
+++ src/main/java/com/gameflix/urjc/memes/GameflixApplication.java
@@ -0,0 +1,12 @@
+package com.gameflix.urjc.memes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameflixApplication.class, args);
+	}
+}
