1661fe1f10d2f4e6f4fefb16788b9e14a9421fb8
Javier <javichi96@gmail.com>
2018-02-21 16:58:46 +0100

    Añadida clase conexión y partidaDemo

--- src/main/java/es/urjc/etsii/PartidaDemo.java
+++ src/main/java/es/urjc/etsii/PartidaDemo.java
@@ -1,6 +1,59 @@
 package es.urjc.etsii;
 
+import sun.security.krb5.internal.PAData;
+
 public class PartidaDemo {
-    private String fecha, hora;
-    
+    private String date;
+    private String time;
+    private Juego game;
+    private Usuario user;
+    private double duration;
+
+    public PartidaDemo(){
+
+    }
+
+    public PartidaDemo(String date, String time, Juego game, Usuario user, double duration){
+        
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Juego getGame() {
+        return game;
+    }
+
+    public void setGame(Juego game) {
+        this.game = game;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
 }
