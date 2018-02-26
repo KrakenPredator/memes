af068034deda64aa5a2e290f065025d6deb3d0cd
Javier <javichi96@gmail.com>
2018-02-26 16:11:20 +0100

    CON REGISTRO

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,4 +1,13 @@
 var sesionIniciada = false;
+
+function logOut() {
+
+    sesionIniciada = false;
+    $('#login-link').replaceWith("<li id=\"login-link\"><a id=\"login-link\" href=\"#modalLogin\" \" data-toggle=\"modal\"><span id=\"sesion-icon\" class=\"glyphicon glyphicon-log-in\"></span> Iniciar sesión</a></li>");
+    $('#profile').replaceWith("<li id=\"profile\" style=\"display:none;\"><a  href=\"#\"><span class=\"glyphicon glyphicon-user\"></span> Perfil</a></li>");
+
+}
+
 function checkear_usuario(){
     var user = {}
     user["username"]=$("#email").val();
@@ -13,10 +22,12 @@ function checkear_usuario(){
         dataType: 'json',
         data: JSON.stringify(user),
         success: function (e) {
-            console.log("Successful Login "+e.username);
-            var profile = "<a id=\"profile\" href=\"#\"><span class=\"glyphicon glyphicon-user\"></span> "+e.name+"</a>"
+            console.log("Successful Login"+e.name+" is inside, we are inside nigga");
+            var profile = "<li id='profile'><a  href='#'><span class='glyphicon glyphicon-user'></span> "+e.name+"</a></li>";
             $('#profile').replaceWith(profile);
             $('#modalLogin').modal('hide');
+            var sesion = "<li id=\"login-link\"><a onclick='logOut()' data-toggle=\"modal\" href='#'><span id=\"sesion-icon\" class=\"glyphicon glyphicon-log-out\"></span> Cerrar sesión</a></li>";
+            $('#login-link').replaceWith(sesion);
         },
         error: function (e) {
            console.log("failure: "+e.responseText);
