5ed937fb7235f9c8e33e25fdfb4f44b3e6fce9c4
Javier <javichi96@gmail.com>
2018-03-10 02:19:32 +0100

    ME RALLE Y LO HE QUITADO

--- src/main/webapp/js/demoHandler.js
+++ src/main/webapp/js/demoHandler.js
@@ -1,22 +1,3 @@
-var api = "AIzaSyC-nQaxVToNcO3Kz8jjvmn5CCtVBPoYEGc";
-
-// Search for a specified string.
 $(document).ready(function search() {
-    gapi.client.set("apiKey", "AIzaSyC-nQaxVToNcO3Kz8jjvmn5CCtVBPoYEGc");
-    gapi.client.load("youtube", "v3", function () {
 
-    });
-    var q = "cod gameplay";
-    var request = gapi.client.youtube.search.list({
-        q: q,
-        part: 'snippet'
-    });
-
-    request.execute(function(response) {
-        var str = JSON.stringify(response.result);
-        console.log(response.result);
-        var video = '<div class="embed-responsive embed-responsive-16by9">\n' +
-            '            <iframe width="1920" height="1080" src="https://www.youtube.com/embed/0AByE1oa84E?autoplay=1&showinfo=0&controls=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>\n' +
-            '        </div>'
-    });
 });
